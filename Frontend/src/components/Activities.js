bdd0459ca8711f5f2396c07735c8652da5b76a10
AntreasCh <antreaschristodoulou11@gmail.com>
2023-03-09 22:22:19 +0000

    Iam the best
    Footer completed

--- Frontend/src/components/Activities.js
+++ Frontend/src/components/Activities.js
@@ -12,28 +12,28 @@ return (
           <h2> IC3 Activities</h2>
           <strong><p>{props.text.data[0].text}</p></strong>
           <p>{props.text.data[1].text}</p>
-          <button ><strong>CONTACT US</strong><br></br> Get Involved with IC3-</button>
+          <button> <a href="/contactus"><strong>CONTACT US</strong><br></br> Get Involved with IC3-</a></button>
 
 
           <strong><h3>Research, Innovation, Education, and Skills / Discover More</h3></strong>
-<div class = "grid-container">
-        <div class="grid-item">
-          <button type = "button" id = "b1" class = "small_btn"><strong>DEMONSTRATORS.</strong><p>Demonstrator projects co-designed, delivered and evaluated by industry, academia
+<div className = "grid-container">
+        <div className="grid-item">
+          <button type = "button" id = "b1" className = "small_btn"><strong>DEMONSTRATORS.</strong><p>Demonstrator projects co-designed, delivered and evaluated by industry, academia
             and public sector partners. Find out More -</p></button>
         </div>
-        <div class="grid-item">
-          <button type = "button" id = "b2" class = "small_btn"><strong>RESEARCH THEMES AND PROJECTS.</strong><p>Reflecting the most pressing issues faced by the sector. Find out more -</p></button>
+        <div className="grid-item">
+          <button type = "button" id = "b2" className = "small_btn"><strong>RESEARCH THEMES AND PROJECTS.</strong><p>Reflecting the most pressing issues faced by the sector. Find out more -</p></button>
         </div>
-        <div class="grid-item">
-          <button type = "button" id = "b3" class = "small_btn"><strong>EDUCATION AND SKILLS .</strong><p> Revitalising the education agenda
+        <div className="grid-item">
+          <button type = "button" id = "b3" className = "small_btn"><strong>EDUCATION AND SKILLS .</strong><p> Revitalising the education agenda
             for construction. Find out more -</p></button>
         </div>
-        <div class="grid-item">
-          <button type = "button" id = "b4" class = "small_btn"><strong>INNOVATION VOUCHERS.</strong><p>An opportunity for funded collaboration
+        <div className="grid-item">
+          <button type = "button" id = "b4" className = "small_btn"><strong>INNOVATION VOUCHERS.</strong><p>An opportunity for funded collaboration
             with our IC3 Partners. Find out more -</p></button>
         </div>
-        <div class="grid-item b5">
-          <button type = "button" id = "b5" class = "big_btn"><strong>Contact Us.</strong><p>Collaborate with us and help us to shape
+        <div className="grid-item b5">
+          <button type = "button" id = "b5" className = "big_btn"><strong>Contact Us.</strong><p>Collaborate with us and help us to shape
             the future of IC3. Find out More -</p></button>
         </div>
       </div>
--- Frontend/src/components/ParentPage.js
+++ Frontend/src/components/ParentPage.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import Navbar from './NavBar';
+import Footer from './Footer';
 const ParentPage = (props) => {
   return (
     <div className="ParentPage">
       <header>
         
         < Navbar/>
-        <br></br>
+   
         
       </header>
 
 
       {props.children}
 
-      <footer>
-        <p>Key Partners:</p>
-        <ul>
-          <li><a href="#key-partners">Partner 1</a></li>
-          <li><a href="#key-partners">Partner 2</a></li>
-          <li><a href="#key-partners">Partner 3</a></li>
-        </ul>
-        <p>Follow us on:</p>
-        <ul>
-          <li><a href="#linkedin">LinkedIn</a></li>
-          <li><a href="#twitter">Twitter</a></li>
-        </ul>
-      </footer>
+      <Footer/>
     </div>
+    
   );
 };
 
